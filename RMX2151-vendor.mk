38e811d86cf740f8b10fa2d27ca690a3219eb760
SamarV-121 <samarvispute121@gmail.com>
2021-04-26 20:16:42 +0530

    RMX2151: Add libvpud_vcodec.so from RMX2151PU_11_A.75
    * To fix video recording as new vendors broke it
    
    Signed-off-by: Sarthak <sarthakroy2002@gmail.com>

--- RMX2151-vendor.mk
+++ RMX2151-vendor.mk
@@ -12,6 +12,7 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX2151/proprietary/lib/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libshowlogo.so \
     vendor/realme/RMX2151/proprietary/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptXHD_encoder.so \
     vendor/realme/RMX2151/proprietary/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptX_encoder.so \
+    vendor/realme/RMX2151/proprietary/product/vendor_overlay/29/lib/libvpud_vcodec.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libvpud_vcodec.so
 
 PRODUCT_PACKAGES += \
     ImsService \
